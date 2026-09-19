e8b6179e6c98dd7733137f4bd3fc19c0d10a2148
Wendell Pereira <r.wendell.regis@gmail.com>
2026-09-19 12:47:14 -0300

    feat: add shell público

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('LASM');
+  protected readonly abas = [
+  { rota: '/inicio', texto: 'Início' },
+  { rota: '/checkin', texto: 'Check-in' },
+  { rota: '/conteudos', texto: 'Conteúdos' },
+  { rota: '/ajuda', texto: 'Preciso de ajuda' },
+];
 }

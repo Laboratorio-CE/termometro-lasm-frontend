c5f983cccee517a1486d6064dda7b17393cbc1f5
Wendell Pereira <r.wendell.regis@gmail.com>
2026-09-19 11:32:04 -0300

    feat: add routes and not-found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'inicio'},
+
+    { path: 'inicio', loadComponent: () => import('./pages/inicio/inicio').then(m => m.Inicio) },
+    { path: 'checkin', loadComponent: () => import('./pages/checkin/checkin').then(m => m.Checkin) },
+    { path: 'conteudos', loadComponent: () => import('./pages/conteudos/conteudos/conteudos').then(m => m.Conteudos) },
+    { path: 'conteudos/:slug', loadComponent: () => import('./pages/conteudos/leitura/leitura').then(m => m.Leitura) },
+    { path: 'ajuda', loadComponent: () => import('./pages/ajuda/ajuda').then(m => m.Ajuda) },
+
+    { path: 'admin/login', loadComponent: () => import('./pages/admin/login/login').then(m => m.Login) },
+    {
+        path: 'admin', loadComponent: () => import('./pages/admin/admin-shell/admin-shell').then(m => m.AdminShell), 
+        children: [
+            { path: '', loadComponent: () => import('./pages/admin/indicadores/indicadores').then(m => m.Indicadores) },
+            { path: 'conteudos', loadComponent: () => import('./pages/admin/conteudos-admin/conteudos-admin').then(m => m.ConteudosAdmin) },
+            { path: 'conteudos/:id', loadComponent: () => import('./pages/admin/conteudos-editor/conteudos-editor').then(m => m.ConteudosEditor) },
+            { path: 'respostas', loadComponent: () => import('./pages/admin/respostas/respostas').then(m => m.Respostas) }
+        ]
+    },
+    { path: '**', loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFound) }
+];

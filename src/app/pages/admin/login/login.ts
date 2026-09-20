c61b4d66b3aa7649e20fb4449ac2696e1c6f2891
Wendell Pereira <r.wendell.regis@gmail.com>
2026-09-20 16:01:57 -0300

    feat add login page and logic
    Criada página de login, junto da lógica de login utilizando os cookies CSRF

--- src/app/pages/admin/login/login.ts
+++ src/app/pages/admin/login/login.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Auth } from '../../../core/api/auth';
 
 @Component({
-  imports: [RouterLink],
+  imports: [FormsModule],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
-export class Login {}
+export class Login implements OnInit {
+  private readonly auth = inject(Auth);
+  private readonly router = inject(Router);
+
+  readonly erro = signal('');
+  readonly enviando = signal(false);
+
+  ngOnInit() {
+    this.auth.carregarSessao().subscribe((usuario) => {
+      if (usuario) {
+        this.router.navigate(['/admin/indicadores']);
+      }
+    });
+  }
+  logar(email: string, senha: string) {
+    this.erro.set('');
+    this.enviando.set(true);
+
+    this.auth.logar(email, senha).subscribe({
+      next: () => 
+        this.auth.carregarSessao().subscribe(() => this.router.navigate(['/admin/indicadores'])),
+        error: () => {
+          this.erro.set('Email ou senha inválidos');
+          this.enviando.set(false);
+        },
+    });
+  }
+}

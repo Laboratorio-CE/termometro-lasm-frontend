9c2b38688cf6855ae130c0c03659320a303e0300
Wendell Pereira <r.wendell.regis@gmail.com>
2026-09-20 15:42:35 -0300

    feat: create login routes
    cria rotas de login, interface para o model usuario, funções logar, deslogar, carregar sessão

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { credenciaisInterceptor } from './core/interceptors/credenciais-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([credenciaisInterceptor]))
   ]
 };
